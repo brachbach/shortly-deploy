887e91561b3f8c797dadfc1185c7e6a41c329df3
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 16:35:47 -0700

    Fix buggy visit numbers

--- app/models/link.js
+++ app/models/link.js
@@ -16,6 +16,7 @@ linksSchema.pre('save', function(next) {
     var shasum = crypto.createHash('sha1');
     shasum.update(this.url);
     this.code = shasum.digest('hex').slice(0, 5);
+    this.visits = 0;
   }
   next(); //try set if doesnt work
 });
