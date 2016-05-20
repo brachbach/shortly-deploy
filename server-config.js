440c93f5337e5d1a612ddfa5fe14b13bd6689a13
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 17:13:26 -0700

    Testing server

--- server-config.js
+++ server-config.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 app.use(cookieParser('shhhh, very secret'));
 app.use(session({
